a55308e0daa18b797c70fc7a51bdebe318ff6b45
sonjiwoo1215 <102014705+sonjiwoo1215@users.noreply.github.com>
2024-01-11 09:28:34 +0900

    Add files via upload

--- 0_sjw/JS/step1/js/1_1_check.js
+++ 0_sjw/JS/step1/js/1_1_check.js
@@ -1,3 +1,12 @@
+// runalert있는 기명함수 안에 alert이라는 라이브러리 함수를 넣음
 function runAlert(){
     alert("hello?");
+}
+// write도 라이브러리 함수 alert랑 사용하는 방식이 각각 다름 대상이 필요 .은 '~에' 라는 뜻
+function runWrite(){
+    document.write('hi'+'<br>'+'js...!');
+}
+// 디버깅 3가지 방법(alert write log) 중 가장 많이 사용
+function runLog(){
+    console.log('hi? js!');
 }
