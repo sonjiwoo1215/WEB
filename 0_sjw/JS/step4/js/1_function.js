53d3f5d0da86c4d75f3c2f04899d666f4d877407
sonjiwoo1215 <102014705+sonjiwoo1215@users.noreply.github.com>
2024-01-22 10:50:16 +0900

    Add files via upload

--- 0_sjw/JS/step4/js/1_function.js
+++ 0_sjw/JS/step4/js/1_function.js
@@ -59,3 +59,41 @@ function showInfo(){
     console.log("1번방 = "+arguments[1]);
     alert(arguments[0]+"의 나이는 "+arguments[1]+"세 입니다.");
 } 
+
+/*
+    ?return (리턴)
+    * 함수 내부는 함수라는 범위 안에 갇히기 때문에, 한번 실행되면 함수 외부에서 접근 불가.
+    * 이 때 매개변수를 활용하면 함수 내부로 data를 전달 할 수 있는데, return value는 이와 반대 되는 값.
+    * 즉, 매개변수 값이 함수 외부에서 내부로 들어오는 입력 부라면, 리턴값은 함수 내부에서 처리한 결과를 함수 외부로 전달하기 위해 사용하는 출력 부.
+
+
+    !ex.1 함수f(x)안에 넣는 값이 매개변수, 결과로 나오는 x*x 가 리턴값.
+    // alert 에서 실행되면 alert으로 반환 실행한곳에서 반환 아예 A,B함수가 있는데 A함수에서 매개변수 받고 실행하고 리턴넣음 B에서 부르면 B함수로 바로 꽂아버림 연결시킴 (함수지향적<-> 객체지향적) 
+        * function f(x) { return x * x; }
+        * alert(f(3));
+
+    !ex.2 alert에서 매개변수를 두개를 선언하면 추가된 매개변수를 무시한다.
+    //*(매개변수가 초과 됐을 시) 또한 원래 함수의 매개변수보다 적게 입력하면 undefined를 출력한다.
+        alert('기존매개변수', '추가매개변수');
+        prompt('기존매개변수');
+*/
+// 이 함수를 실행하는 위치가 콘솔 콘솔로 바로 값을 던짐 콘솔에서 매개변수 넣으면 실시간으로 대응됨 alert에서 함수를 실행하면 콘솔창이 아니라 alert으로 날라가서 반환값 보여줌 alert(30);이나 마찬가지 ㅇㅇ 이 함수를 호출한 부분으로 return
+function sumReturn1(num1,num2){
+    var result = num1 + num2;
+    return result;
+}
+function infiniteSum(){
+    var sum = 0; //?
+    var count = 1; //?
+    while(true){ //?무한반복
+        var value=parseInt(window.prompt("숫자만 입력해라"));
+        if(value==0){
+            document.write("종료");
+            return;
+        }
+        sum += value;
+        document.write(count+"."+sum+"<br>");
+        count++;
+    }
+    document.write("총"+count+"번 실행함"); //?
+}
