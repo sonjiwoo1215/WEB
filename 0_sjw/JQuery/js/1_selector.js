da669e3f6b3d61218eff61162fa6fa37df7cbb51
sonjiwoo1215 <102014705+sonjiwoo1215@users.noreply.github.com>
2024-01-24 10:50:38 +0900

    Add files via upload

--- 0_sjw/JQuery/js/1_selector.js
+++ 0_sjw/JQuery/js/1_selector.js
@@ -1,7 +1,33 @@
 // js문법
 window.onload
 
-// 제이쿼리의 seperator html문서자ㅔ체를 document(브라우저)라 부름
+// $ 제이쿼리의 seperator (제이쿼리문법을 알려주는 식별자) html문서자체를 document(브라우저)라 부름 제이쿼리문법을 쓰려면 이 안에 써야함
+// 준비되었을때
 $(document).ready(function(){
+    // html에 모든 div들이 이 변수에 담김 제이쿼리는 css선택자랑 같은거 사용가능
+    // !case.1
+    var $divs = $("div");
+    $divs.css("color","#f00");
 
+    // js에서는 onclick 쓰거나 .on(click,function())
+    // ? case.2
+    $("#btnCheck").click(sayHello);
+
+    // ?case.3
+    $("#btnAddBorder").click(addBorder);
 });
+// $(function(){
+
+// });
+// *case.2
+function sayHello(){
+    alert('hello');
+}
+// 얘네들 case1쪽 안에 넣으면 안됨?? 함수밖에 만들고 저 안에 클릭되면 뜨게하는건 안에 쓰기 
+// 스크립트로 넣는 css / 이렇게 직접 5px어쩌고 넣는것보다 css에서 클래스로 넣고 스크립트에서 클래스 갖고놀기
+// *case.3
+function addBorder(){
+    // js는 document.body.getElement 어쩌고 해야함
+    // 매개변수는 이름이랑 값 두개
+    $("#panel").css("border","5px solid black");
+}
