29f21bff59b7611db57dfc9f4d342ec1812364f6
sonjiwoo1215 <102014705+sonjiwoo1215@users.noreply.github.com>
2024-01-26 10:07:09 +0900

    Add files via upload

---
+++ 0_sjw/JQuery/js/3_addRemoveToggle.js
@@ -0,0 +1,46 @@
+// !아이콘스 심볼 cdn
+$(document).ready(function(){
+    // *case.1 checkBox 만들기
+    toggleCheck();
+    // todo passwordComponent
+    toggleTodo();
+});
+// true false 왔다갔다하는 행위를 toggle
+function toggleCheck(){
+    // cdn 방식
+    var $checkTarget=$(".material-icons.checkIco");
+    var $checkLabel=$(".label.checkLabel");
+    // 어떤 상황일때는 빠지고 어떤 상황일때는 들어와라라는걸 저장하는 곳
+    var checkStatus=false;
+    $checkLabel.click(function(){
+        // 느낌표 위치 != 같지 않다 변수앞에 ! 붙으면 반대 / 이건 js방식임 제이쿼리는 토글이란 함수가 있음
+        checkStatus= !checkStatus;
+        if(checkStatus==true){
+            // 제이쿼리는 직관적 제이쿼리에서 글자를 바꾸는 함수는 text ()안에 아무것도 없으면 앞에 대상의 글자를 가져와라 글자를 넣으면 바꿔끼워라
+            $checkTarget.text("check_box");
+        }
+        else{
+            $checkTarget.text("check_box_outline_blank");
+        }
+        
+    });
+}
+
+function toggleTodo(){
+    // password -> text(input type) : $(대상).attr("속성명-type","속성값-password");
+    var $toggleTarget=$(".material-icons.toggleVisible");
+    var $toggleInput=$("#userPW");
+    var toggleStatus=false;
+
+    $toggleTarget.click(function(){
+        toggleStatus= !toggleStatus;
+        if(toggleStatus==true){
+            $toggleTarget.text("visibility");
+            $toggleInput.attr("type","text");
+        }
+        else{
+            $toggleTarget.text("visibility_off");
+            $toggleInput.attr("type","password");
+        }
+    });
+}
