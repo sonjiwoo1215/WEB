93e8c61cc71d6682dde06dc681371fda3d969ede
sonjiwoo1215 <102014705+sonjiwoo1215@users.noreply.github.com>
2024-01-31 09:09:12 +0900

    Add files via upload

--- 0_sjw/JQuery/js/5_foldTODO.js
+++ 0_sjw/JQuery/js/5_foldTODO.js
@@ -1,31 +1,70 @@
 $(document).ready(function(){
-    gallery(".1way");
+    // gallery(".1way");
     // todo case.1 매개변수 이용 - 클릭이 일어날 때 해당 열, 너비, 높이를 함수에 전달
     // todo case.2 매개변수를 사용하지 않고 한번에 해결하는 방법 (replace(); 함수 찾아적용)
+    // replace() 대상을 찾아서 대체해줌 way만 변경함 1은 냅두는것/  replaceAll은 싹 다 변경
 });
-function gallery(but){
-    var $col=null; //열 
-    var wid=0;//너비
-    var hei=0; //높이
-    $(but).click(function(){
-        var imgLength=img.length;
-        var imgHeight=img.height();
 
-// ? 클릭한 버튼이 1열인지 2열인지 어떻게 알게하지..?
-        for(var i=0; i<imgLength; i++){ //이미지 0~19 반복
-            var image=img.eq(i); // i번째 이미지
-            wid=1000/(i+1);
-            hei=1000/(i+1);
-            image.css({
-                "width":wid,
-                "height":hei
-            });
+$(document).ready(function(){
+    /*
+    gallery2(0,200,200);
+    $(".1way").click(function(){gallery2(1,1000,1000)});
+    $(".2way").click(function(){gallery2(2,500,500)});
+    $(".3way").click(function(){gallery2(3,333,333)});
+    $(".4way").click(function(){gallery2(4,250,250)});
+    $(".5way").click(function(){gallery2(5,200,200)});
+    $(".6way").click(function(){gallery2(6,166,166)});
+    $(".7way").click(function(){gallery2(7,142,142)});
+    $(".8way").click(function(){gallery2(8,125,125)});
+    $(".9way").click(function(){gallery2(9,111,111)});
+    $(".10way").click(function(){gallery2(10,100,100)});*/
+    // todo case.1 parameter를 이용한 방법. - click이 일어날 때 해당 열,너비,높이를 함수에 전달.
+    
+    gallery();
+    // todo case.2 parameter를 사용하지 않고 한번에 해결하는 방법. - .replace(); 함수 찾아적용.
+});
+
+
+function gallery(){
+    var currentWay,xpos, ypos;
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+    var containerW = $("#imgContainer").width();
+    var imgSize = $images.width();
+
+    $("[class$='way']").click(function(){
+        currentWay = parseInt($(this).attr("class").replace("way",""));
+        for(i = 0; i<$imgLength; i++){
+            var $image = $images.eq(i); //? eq = DOM 요소 세트를 나타내는 jQuery 객체가 주어지면 .eq()메소드는 해당 세트 내의 한 요소에서 새 jQuery 객체를 구성한다.
+            imgSize = containerW / currentWay;
+            xpos = (i % currentWay) * imgSize;
+            ypos = parseInt(i / currentWay) * imgSize;
+            
+            $image.css({
+                'left' : xpos,
+                'top' : ypos,
+                'width' : imgSize,
+                'height' : imgSize
+            })
         }
     });
-    $col=$(this);
 }
-// 0~19 반복
-// 1열 너비1000 높이1000 1개 / 1000/1 / 0번째이미지 1000
-// 2열 너비500 높이500 2개 / 1000/2 / 01번째 이미지 500
-// 3열 너비333 높이333 3개 / 1000/3 / 012번째 이미지 333
-// 4열 너비250 높이250 4개 / 1000/4 / 0123번째 이미지 250
+function gallery2(count, imgWidth, imgHeight){
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+    var xpos, ypos;
+
+    for(i = 0; i<$imgLength; i++){
+        var $image = $images.eq(i); //? eq = DOM 요소 세트를 나타내는 jQuery 객체가 주어지면 .eq()메소드는 해당 세트 내의 한 요소에서 새 jQuery 객체를 구성한다.
+        
+        xpos = (i % count) * imgWidth;
+        ypos = parseInt(i / count) * imgHeight;
+
+        $image.css({
+            'left' : xpos,
+            'top' : ypos,
+            'width' : imgWidth,
+            'height' : imgHeight
+        })
+    }
+}
