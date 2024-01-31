06992546541e11e62fa64f0067d2b4f5c6272468
sonjiwoo1215 <102014705+sonjiwoo1215@users.noreply.github.com>
2024-01-31 09:19:59 +0900

    Add files via upload

--- 0_sjw/JQuery/js/9_randomPos.js
+++ 0_sjw/JQuery/js/9_randomPos.js
@@ -5,7 +5,7 @@ var playState=true;
 var $btnStart=null; //? 게임시작 버튼
 var $panel=null; //? frame 
 var timerID=0; //?
-
+// 지역변수쓰면 리턴해야되고 다른함수에서는 매개변수쓰고 그걸 통해 받아야함 그게 귀찮아서 전역변수
 $(document).ready(function(){
     init();
     initEvent();
@@ -27,6 +27,7 @@ function gameStart(){
     if(playState==true){
         // 게임시작과 동시에 카운트가 들어가니까 종료함수도 동시에 실행
         gameEnd();
+        // 0.5초마다 left,top을 구한다 (동시에는 못하고 left먼저)
         timerID=setInterval(moveCircle,500);
     }
 }
@@ -36,16 +37,26 @@ function gameStart(){
 function moveCircle(){
     $circle.css({
         left: Math.floor(Math.random()*($panel.width()-$circle.width())),
-        top:Math.floor(Math.random()*($panel.height()-$circle.height()))
+        top: Math.floor(Math.random()*($panel.height()-$circle.height()))
     })
 }
 // ? 점수집계함수
 function scoreCount(){
-
+    if(playState==true){
+        count++;
+        $score.text(count);
+    }
 }
 // ? 게임종료 함수
 // 이미 실행되서 움직이고 있는 함수는 중간에 가져올 수가 없음 그래서 셋인터벌을 변수에 담고 그 변수를 매개변수에 담아 끌어오기
 function gameEnd(){
-    // setInterval취소
+    setTimeout(function(){
+    playState=false;
+    // setInterval취소 / 클리어인터벌 실행될건데 10초뒤에 
     clearInterval(timerID);
+    alert("게임종료. 너의 점수는 "+count+"점이다.");
+    count=0;
+    $score.text(count);
+    },10000);
+
 }
